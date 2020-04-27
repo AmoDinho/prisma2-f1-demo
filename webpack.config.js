6d310ba3748b247f8aa8ac4dcb731001fc4ae18f
AmoDinho <u14284783@tuks.co.za>
2020-04-27 20:15:26 +0200

    added copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const slsw = require("serverless-webpack");
 const nodeExternals = require("webpack-node-externals");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: slsw.lib.entries,
