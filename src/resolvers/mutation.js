ee37b68b2b50fadc613078dd664e06812a953536
AmoDinho <u14284783@tuks.co.za>
2020-04-27 18:43:44 +0200

    create mutation

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -2,5 +2,17 @@ import { PrimsaClient, PrismaClient } from "@prisma/client";
 
 const Primsa = new PrismaClient();
 export const createATeam = async (args, context) => {
+  const team = await Primsa.team.create({
+    data: {
+      name: args.name,
+      base: args.base,
+      team_principal: args.team_principal,
+      power_unit: args.power_unit,
+      world_championships: args.world_championships,
+    },
+  });
+
+  console.log(team);
+
   return null;
 };
